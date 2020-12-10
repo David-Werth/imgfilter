a120d6b18632fcf3ca306fab8b39f4920a9756f9
David-Werth <75678655+David-Werth@users.noreply.github.com>
2020-12-10 13:18:35 +0100

    updated an img file to test filter bypass attempt.

--- script.js
+++ script.js
@@ -1,63 +1,66 @@
 // Example of a database with blocked bank accounts
 
 const blockedBankAccounts = [
-    "97500105174735761646",
-    "74500105173475126198",
-    "75500105171827358468",
-    "741243438939515813",
-    "4850519824981492784396"
+  "97500105174735761646",
+  "74500105173475126198",
+  "75500105171827358468",
+  "741243438939515813",
+  "4850519824981492784396",
 ];
 
-
 const img = document.getElementById("uplimg");
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn");
 const img2 = document.getElementById("uplimg2");
-const btn2 = document.getElementById("btn2")
+const btn2 = document.getElementById("btn2");
 const img3 = document.getElementById("uplimg3");
-const btn3 = document.getElementById("btn3")
+const btn3 = document.getElementById("btn3");
 const img4 = document.getElementById("uplimg4");
-const btn4 = document.getElementById("btn4")
-
+const btn4 = document.getElementById("btn4");
 const p = document.createElement("P");
-
+const loading = document.getElementById("loading");
 
 btn.addEventListener("click", () => {
-    checkImg(img);
-})
+  checkImg(img);
+});
 
 btn2.addEventListener("click", () => {
-    checkImg(img2);
-})
+  checkImg(img2);
+});
 
 btn3.addEventListener("click", () => {
-    checkImg(img3);
-})
+  checkImg(img3);
+});
 
 btn4.addEventListener("click", () => {
-    checkImg(img4);
-})
+  checkImg(img4);
+});
 
-function checkImg (img) {
-    let textFromImg = [];
-    Tesseract.recognize(
+function checkImg(img) {
+  loading.style.display = "inline";
+  let textFromImg = [];
+  Tesseract.recognize(
     img,
-    'eng',
+    "eng"
     // { logger: m => console.log(m) }
-    ).then(({ data: { text } }) => {
-        textFromImg.push(text.replace(/[^0-9]/g, ''));
-        textFromImg = textFromImg.toString();
-                
-        function result () {
-            for (element of blockedBankAccounts) {
-                if (textFromImg === element) {
-                    img.style.border = "4px solid red";
-                    img.parentElement.appendChild(p).innerHTML = `Blocked Bank Account: ${textFromImg}`;
-                    return;
-                }
-            }
-            img.style.border = "4px solid green";
-             img.parentElement.appendChild(p).innerHTML = `Clean Bank Account: ${textFromImg}`;
-        } 
-        result();
-    })
+  ).then(({ data: { text } }) => {
+    textFromImg.push(text.replace(/[^0-9]/g, ""));
+    textFromImg = textFromImg.toString();
+    function result() {
+      for (element of blockedBankAccounts) {
+        if (textFromImg === element) {
+          img.style.border = "4px solid red";
+          img.parentElement.appendChild(
+            p
+          ).innerHTML = `Blocked Bank Account: ${textFromImg}`;
+          return;
+        }
+      }
+      img.style.border = "4px solid green";
+      img.parentElement.appendChild(
+        p
+      ).innerHTML = `Clean Bank Account: ${textFromImg}`;
+    }
+    result();
+    loading.style.display = "none";
+  });
 }
